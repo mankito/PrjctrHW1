c36a32cbe389a3ebdd39ff80e4cfbb4ebbce4d2b
Kostiantyn Tkachenko <britney.spears.fan.number.one@gmail.com>
2023-06-20 23:19:30 +0200

    Optimize Task1.

--- app.js
+++ app.js
@@ -1,24 +1,20 @@
 const userNames = ["Петрик Ольга Іванівна", "Гнатюк Петро Антонович", "Рудко Андрій Опанасович"];
-let initials = [];
-for (let i = 0; i < userNames.length; i++) {
- 	let name = userNames[i].split(' ');
-  	let initial = '';
-  	for (let j = 0; j < name.length; j++) {
-  		initial += name[j].substring(0, 1) + '.';
-	}
-  initials.push(initial);
-}
-initials.sort();
+let initials = userNames.map(name => name.split(' ').map(word => word[0]).join('.') + '.').sort();
+
 console.log(initials); // [ "Г.П.А.", "П.О.І.", "Р.А.О."]
-//Відчуваю, що це не найкращий спосіб, але через проблеми зі здоров'ям не встигаю зробити нічого ліпшого. 
-//Сподіваюсь, цього достатньо для зарахування. Пізніше хотів би написати щось лаконічніше.
 
-const currentMaxValue = 4589;
-let reverseMaxValue = currentMaxValue.toString().split('').reverse().join('');
-reverseMaxValue = Number(reverseMaxValue);
-console.log(reverseMaxValue); // 9854
-console.log(typeof reverseMaxValue); // 'number'
 
-const resultsArray = [1, 2, [3, [4]]];
-let productOfArray = resultsArray.flat(Infinity).reduce((total, currentVal) => total * currentVal);
-console.log(productOfArray); // 24
+//Task2
+
+// const currentMaxValue = 4589;
+// let reverseMaxValue = currentMaxValue.toString().split('').reverse().join('');
+// reverseMaxValue = Number(reverseMaxValue);
+// console.log(reverseMaxValue); // 9854
+// console.log(typeof reverseMaxValue); // 'number'
+
+
+//Task3
+
+// const resultsArray = [1, 2, [3, [4]]];
+// let productOfArray = resultsArray.flat(Infinity).reduce((total, currentVal) => total * currentVal);
+// console.log(productOfArray); // 24
